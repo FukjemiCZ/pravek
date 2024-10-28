f42ef16d76d6ba300e2d445ac37b8c120669b4f3
FukjemiCZ <github@fukjemi.cz>
2024-10-28 12:51:20 +0100

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/blog">
             Přihlásit se - 5min ⏱️
           </Link>
         </div>
